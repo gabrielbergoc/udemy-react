95ce657ee037af5b5b9740aa7dfc2476bc46c1a8
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2023-02-15 23:29:48 -0300

    pagination

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+const Button = ({ text, type, onClick, disabled }) => (
+  <button type={type} onClick={onClick} className="button" disabled={disabled}>{text}</button>
+);
+
+export default Button;
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,32 +1,66 @@
 import { Component } from "react";
 import "./styles.css";
 import { getPosts } from "../../utils";
-import Posts from '../../components/Posts';
+import Posts from "../../components/Posts";
+import Button from "../../components/Button";
 
 class Home extends Component {
   state = {
     posts: [],
+    allPosts: [],
+    page: 0,
+    pageSize: 6,
+    initialPageSize: 6,
+    noMorePosts: false,
   };
 
   // executed right after the component is first rendered in the DOM
-  componentDidMount() {
-    this.setPosts();
+  async componentDidMount() {
+    await this.loadAllPosts();
+    this.paginate();
   }
 
   // executed right before the component is destroyed
   componentWillUnmount() {}
 
-  setPosts = async () => {
-    const posts = await getPosts();
-    this.setState({ posts });
+  loadAllPosts = async () => {
+    const allPosts = await getPosts();
+    this.setState({ allPosts });
+  };
+
+  extendPage = () => {
+    const { pageSize, initialPageSize } = this.state;
+    this.setState({ pageSize: pageSize + initialPageSize });
+  };
+
+  paginate = () => {
+    const { page, pageSize, allPosts } = this.state;
+    const start = page * pageSize;
+    const end = start + pageSize;
+
+    let noMorePosts = false;
+    if (end >= allPosts.length) {
+      noMorePosts = true;
+    }
+
+    this.setState({ posts: allPosts.slice(start, end), noMorePosts });
+  };
+
+  handleButton = () => {
+    this.extendPage();
+    this.paginate();
   };
 
   render() {
-    const { posts } = this.state;
+    const { posts, noMorePosts } = this.state;
 
     return (
       <section className="container">
         <Posts posts={posts} />
+
+        <div className="button-container">
+          <Button onClick={this.handleButton} disabled={noMorePosts} text="More posts..." />
+        </div>
       </section>
     );
   }
