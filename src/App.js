55f497087275525b205d69943ea7d985ca6304a8
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2023-02-15 22:11:43 -0300

    fetch posts from mock api

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   state = {
-    name: 'gabs',
-    names: ['bergs', 'gabs'],
-    posts: [
-      {
-        id: 1,
-        title: 'title 1',
-        body: 'body 1',
-      },
-      {
-        id: 2,
-        title: 'title 2',
-        body: 'body 2',
-      },
-      {
-        id: 3,
-        title: 'title 3',
-        body: 'body 3',
-      },
-    ],
+    posts: [],
   };
 
   // executed right after the component is first rendered in the DOM
   componentDidMount() {
-
+    this.getPosts();
   }
 
   // executed right before the component is destroyed
-  componentWillUnmount() {
+  componentWillUnmount() {}
 
-  }
+  getPosts = async () => {
+    const postsFetch = fetch("https://jsonplaceholder.typicode.com/posts");
+    const photosFetch = fetch("https://jsonplaceholder.typicode.com/photos");
+
+    const [postsResponse, photosResponse] = await Promise.all([
+      postsFetch,
+      photosFetch,
+    ]);
+    const [postsJson, photosJson] = await Promise.all([
+      postsResponse.json(),
+      photosResponse.json(),
+    ]);
+
+    const posts = postsJson.map((post, i) => ({
+      ...post,
+      cover: photosJson[i],
+    }));
+
+    this.setState({ posts });
+  };
 
   render() {
     const { posts } = this.state;
     return (
-      <div className="App">
-        {posts.map(post => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </div>  
+      <div className="posts">
+        {posts.map((post) => (
+          <div key={post.id} className="post">
+            <div className="post-content">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
         ))}
       </div>
     );
