a46b565fe38e19ed8c73017b71d2172d8b936227
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2023-02-15 22:06:27 -0300

    App.js: change default react app page for custom

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 import { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   state = {
     name: 'gabs',
+    names: ['bergs', 'gabs'],
+    posts: [
+      {
+        id: 1,
+        title: 'title 1',
+        body: 'body 1',
+      },
+      {
+        id: 2,
+        title: 'title 2',
+        body: 'body 2',
+      },
+      {
+        id: 3,
+        title: 'title 3',
+        body: 'body 3',
+      },
+    ],
   };
 
-  // don't need constructor if class fields and arrow function methods are used
-  // constructor(props) {
-  //   super(props);
-  //   // this.handleClick = this.handleClick.bind(this); --> don't need to do this
-  //   // if function is declared as arrow function
-  // }
+  // executed right after the component is first rendered in the DOM
+  componentDidMount() {
 
-  handleClick = () => {
-    console.log(`${this.state.name} clicked`);
-    this.setState({ name: 'bergs' });
+  }
+
+  // executed right before the component is destroyed
+  componentWillUnmount() {
+
   }
 
   render() {
+    const { posts } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleClick}>
-            Hello, {this.state.name}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>  
+        ))}
       </div>
     );
   }
