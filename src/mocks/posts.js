8dbf42f1abecc0cfba1b35518a7f17d0fb8fab94
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2023-02-24 00:33:52 -0300

    Home unit tests

--- src/mocks/posts.js
+++ src/mocks/posts.js
@@ -1,22 +1,143 @@
-export const posts = [
+export const rawPosts = [
   {
+    userId: 1,
     id: 1,
-    title: 'post 1 title',
-    body: 'post 1 body',
-    cover: 'img/img1.png',
+    title: 'post #1 title',
+    body: 'post #1 body',
   },
   {
+    userId: 1,
     id: 2,
-    title: 'post 2 title',
-    body: 'post 2 body',
-    cover: 'img/img2.png',
+    title: 'post #2 title',
+    body: 'post #2 body',
   },
   {
+    userId: 1,
     id: 3,
-    title: 'post 3 title',
-    body: 'post 3 body',
-    cover: 'img/img3.png',
+    title: 'post #3 title',
+    body: 'post #3 body',
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: 'post #4 title',
+    body: 'post #4 body',
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: 'post #5 title',
+    body: 'post #5 body',
+  },
+  {
+    userId: 1,
+    id: 6,
+    title: 'post #6 title',
+    body: 'post #6 body',
+  },
+  {
+    userId: 1,
+    id: 7,
+    title: 'post #7 title',
+    body: 'post #7 body',
+  },
+  {
+    userId: 1,
+    id: 8,
+    title: 'post #8 title',
+    body: 'post #8 body',
+  },
+  {
+    userId: 1,
+    id: 9,
+    title: 'post #9 title',
+    body: 'post #9 body',
+  },
+  {
+    userId: 1,
+    id: 10,
+    title: 'post #10 title',
+    body: 'post #10 body',
   },
 ];
+
+export const photos = [
+  {
+    albumId: 1,
+    id: 1,
+    title: 'photo #1 title',
+    url: 'img/img1.png',
+    thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+  },
+  {
+    albumId: 1,
+    id: 2,
+    title: 'photo #2 title',
+    url: 'img/img2.png',
+    thumbnailUrl: 'https://via.placeholder.com/150/771796',
+  },
+  {
+    albumId: 1,
+    id: 3,
+    title: 'photo #3 title',
+    url: 'img/img3.png',
+    thumbnailUrl: 'https://via.placeholder.com/150/24f355',
+  },
+  {
+    albumId: 1,
+    id: 4,
+    title: 'photo #4 title',
+    url: 'img/img4.png',
+    thumbnailUrl: 'https://via.placeholder.com/150/d32776',
+  },
+  {
+    albumId: 1,
+    id: 5,
+    title: 'photo #5 title',
+    url: 'img/img5.png',
+    thumbnailUrl: 'https://via.placeholder.com/150/f66b97',
+  },
+  {
+    albumId: 1,
+    id: 6,
+    title: 'photo #6 title',
+    url: 'img/img6.png',
+    thumbnailUrl: 'https://via.placeholder.com/150/56a8c2',
+  },
+  {
+    albumId: 1,
+    id: 7,
+    title: 'photo #7 title',
+    url: 'img/img7.png',
+    thumbnailUrl: 'https://via.placeholder.com/150/b0f7cc',
+  },
+  {
+    albumId: 1,
+    id: 8,
+    title: 'photo #8 title',
+    url: 'img/img8.png',
+    thumbnailUrl: 'https://via.placeholder.com/150/54176f',
+  },
+  {
+    albumId: 1,
+    id: 9,
+    title: 'photo #9 title',
+    url: 'img/img9.png',
+    thumbnailUrl: 'https://via.placeholder.com/150/51aa97',
+  },
+  {
+    albumId: 1,
+    id: 10,
+    title: 'photo #10 title',
+    url: 'img/img10.png',
+    thumbnailUrl: 'https://via.placeholder.com/150/810b14',
+  },
+];
+
+export const posts = rawPosts.map((post, i) => {
+  const postWithCover = { ...post };
+  postWithCover.cover = photos[i].url;
+  return postWithCover;
+});
 
 export const post = posts[0];
--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -1,10 +1,99 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import Home from '.';
+import { photos, rawPosts } from '../../mocks/posts';
+
+const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => res(ctx.json(rawPosts))),
+  rest.get('https://jsonplaceholder.typicode.com/photos', async (req, res, ctx) => res(ctx.json(photos))),
+];
+
+const server = setupServer(...handlers);
 
 describe('<Home />', () => {
-  it('renders a container', () => {
-    render(<Home />);
-    const homeContainer = screen.getByTestId('home-container');
-    expect(homeContainer).toBeInTheDocument();
+  beforeAll(() => server.listen());
+
+  beforeEach(() => render(<Home />));
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => server.close());
+
+  it('should render search input, posts and button', async () => {
+    const input = screen.getByPlaceholderText(/search/i);
+    expect(input).toBeInTheDocument();
+    const button = screen.getByText(/more/i);
+    expect(button).toBeInTheDocument();
+    const noMorePosts = screen.getByText(/no.+posts/i);
+    expect(noMorePosts).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(noMorePosts);
+    expect(noMorePosts).not.toBeInTheDocument();
+
+    const imgs = screen.getAllByRole('img', { name: /title/i });
+    expect(imgs).toHaveLength(6);
+  });
+
+  it('should search for posts', async () => {
+    // fetching posts (mock)
+    const noMorePosts = screen.getByText(/no.+posts/i);
+    await waitForElementToBeRemoved(noMorePosts);
+
+    // verifying that only 6 posts are rendered
+    const headings = screen.getAllByText(/title/i);
+    expect(headings).toHaveLength(6);
+
+    // search for posts with certain text (not any of the original 6)
+    const searchInput = screen.getByPlaceholderText(/search/i);
+    userEvent.type(searchInput, '7');
+
+    // verifying that the original 6 posts aren't shown
+    headings.forEach((heading) => expect(heading).not.toBeInTheDocument());
+
+    // verifying that the search was made
+    const newHeadings = screen.getAllByText(/title/i);
+    expect(newHeadings).toHaveLength(1);
+    expect(newHeadings[0].innerHTML).toContain('7');
+  });
+
+  it('should show "no more posts" if search string doesn\'t have a match', async () => {
+    // fetching posts (mock)
+    let noMorePosts = screen.getByText(/no.+posts/i);
+    await waitForElementToBeRemoved(noMorePosts);
+
+    // verifying that only 6 posts are rendered
+    const headings = screen.getAllByText(/title/i);
+    expect(headings).toHaveLength(6);
+
+    // search for posts with certain text (not any of the original 6)
+    const searchInput = screen.getByPlaceholderText(/search/i);
+    userEvent.type(searchInput, '3.1415');
+
+    // verifying that the original 6 posts aren't shown
+    headings.forEach((heading) => expect(heading).not.toBeInTheDocument());
+
+    // verifying that the search was made
+    const newHeadings = screen.queryAllByText(/title/i);
+    expect(newHeadings).toHaveLength(0);
+
+    // verifying that the "no posts match" message is shown
+    noMorePosts = screen.getByText(/no.+posts/i);
+    expect(noMorePosts).toBeInTheDocument();
+  });
+
+  it('should show more posts if "load more" is clicked and disable button if there aren\' more posts', async () => {
+    // fetching posts (mock)
+    let noMorePosts = screen.getByText(/no.+posts/i);
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const button = screen.getByText(/more/i);
+    userEvent.click(button);
+
+    // verifying that ALL posts are rendered and the button is disabled
+    const headings = screen.getAllByText(/title/i);
+    expect(headings).toHaveLength(10);
+    expect(button).toBeDisabled();
   });
 });
